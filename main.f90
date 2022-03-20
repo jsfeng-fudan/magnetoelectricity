3173d46547c5744c18aa38a96055a4d93cacaaaa
Junsheng Feng <fjs@hfnu.edu.cn>
2022-03-20 22:47:02 +0800

    fortran

---
+++ main.f90
@@ -0,0 +1,18 @@
+    ! this code as entry for magnetoelectricity(ME) coefficients  
+    ! here, it is possible to handle ME coefficients without resorting to DFT
+    ! Author : Junsheng Feng
+    ! affilication : Hefei normal Universtiy or Fudan University
+    ! versions : 1.0
+    program main
+
+    implicit none
+
+    write(*,*) "Hello, ME!"
+
+    write(*,*) "Reading tensor from files"
+
+    
+    
+    
+
+    end program main 
